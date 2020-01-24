5ac19b56b19fc59f85297f683407bee6bedd5932
lewismunyi <lewismunyi97@gmail.com>
2020-01-24 23:34:05 +0300

    Configs

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+	mode: "universal",
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: process.env.npm_package_name || "",
+		meta: [
+			{ charset: "utf-8" },
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1"
+			},
+			{
+				hid: "description",
+				name: "description",
+				content: process.env.npm_package_description || ""
+			}
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+	},
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: "#fff" },
+	/*
+	 ** Global CSS
+	 */
+	css: [],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
+	plugins: [],
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [
+		// Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+		"@nuxtjs/tailwindcss"
+	],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		"@nuxtjs/axios",
+		"@nuxtjs/pwa",
+		// Doc: https://github.com/nuxt-community/dotenv-module
+		"@nuxtjs/dotenv",
+		"nuxt-purgecss"
+	],
+	router: {
+		middleware: ["auth"]
+	},
+	/*
+	 ** Axios module configuration
+	 ** See https://axios.nuxtjs.org/options
+	 */
+	axios: {},
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		extractCSS: true,
+		postcss: {
+			plugins: {
+				tailwindcss: "./tailwind.config.js"
+			}
+		},
+		purgeCSS: {
+			mode: "postcss"
+		},
+
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {}
+	}
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/c onfiguration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {},
+  variants: {},
+  plugins: []
+}
