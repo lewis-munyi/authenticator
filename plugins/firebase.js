f4ce265acc7438cdf620200b9341c8380867b4ed
Lewis Munyi <lewismunyi97@gmail.com>
2020-03-28 00:17:59 +0300

    Moved variables to ENV
    Signed-off-by: Lewis Munyi <lewismunyi97@gmail.com>

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,16 +2,15 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCkBxFFVY6CrEzxs5klvZTT7Hyj-WEnVT0",
-	authDomain: "authenticator-web.firebaseapp.com",
-	databaseURL: "https://authenticator-web.firebaseio.com",
-	projectId: "authenticator-web",
-	storageBucket: "authenticator-web.appspot.com",
-	messagingSenderId: "251471237512",
-	appId: "1:251471237512:web:603d8647351eb8af006a3d",
-	measurementId: "G-JNM4ZF9WJY"
+	apiKey: process.env.API_KEY ,
+	authDomain: process.env.AUTH_DOMAIN ,
+	databaseURL: process.env.DATABASE_URL ,
+	projectId: process.env.PROJECT_ID ,
+	storageBucket: process.env.STORAGE_BUCKET ,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID ,
+	appId: process.env.APP_ID ,
+	measurementId: process.env.MEASUREMENT_ID ,
 };
-
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : "";
 
 export const provider = new firebase.auth.GoogleAuthProvider();
